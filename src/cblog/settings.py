b76954df95554418a90997e972cb50b8873f20eb
f4323-Aziz <109878113+f4323-Aziz@users.noreply.github.com>
2022-10-18 22:49:43 +0300

    Add files via upload

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aws-capstone-rds', # database name in RDS is written here
+        'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
         'HOST': 'aws-capstone-rds.cl17m16gjspq.us-east-1.rds.amazonaws.com',  # database endpoint is written here
